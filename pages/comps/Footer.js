d3df8df029ca093f6f5b79f5d919ba6d3a547a57
svetlana.shkarina <sveta_gorodzha@mail.ru>
2021-04-15 20:19:08 +0200

    Add footer component

---
+++ pages/comps/Footer.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <Link href="/">
+        <a className="logo2">Alexandra Newman </a>
+      </Link>
+    </div>
+  );
+};
+
+export default Footer;
