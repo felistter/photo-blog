2a61f45e330a45486bce428e2f47693052ee6e58
svetlana.shkarina <sveta_gorodzha@mail.ru>
2021-04-15 20:17:22 +0200

    Add about section component

---
+++ pages/comps/About.js
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <section className="featured">
+      <span className="title">About me</span>
+      <span className="subtitle">Hi, my name is Alexandra!</span>
+      <span className="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nobis
+        ex aliquid, quae dolor odit veritatis quos quisquam, laboriosam commodi
+        in quibusdam! Asperiores maiores tenetur nihil dolores amet architecto
+        iusto.
+      </span>
+
+      <img className="portrait" src="images/portrait.png" alt="portrait" />
+
+      <img
+        className="circles-middle"
+        src="images/circles-middle.png"
+        alt="circles-middle"
+      />
+    </section>
+  );
+};
+
+export default About;
