e8c568ed9dcef65ca6ac1658a6b0d43d06c6405d
Svetlana Shkarina <sveta_gorodzha@mail.ru>
2021-05-01 22:31:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import About from "../app/components/About";
 import Hero from "../app/components/Hero";
 import GalleryList from "../app/components/GalleryList";
+import Head from 'next/head';
 
 let client = require("contentful").createClient({
   space: process.env.NEXT_CONTENTFUL_SPACE_ID,
@@ -22,6 +23,14 @@ export async function getStaticProps() {
 export default function Home({ articles }) {
   return (
     <div className="content-container">
+      <Head>
+        <meta name="description" content="Personal blog for public sharing travel experience and memories" />
+        <meta name="title" property="og:title" content="Travel Blog" />
+        <meta property="og:type" content="Website" />
+        <meta name="image" property="og:image" content="https://github.com/felistter/portfolio/blob/main/public/work-example-1.png?raw=true" />
+        <meta name="description" property="og:description" content="Personal blog for public sharing travel experience and memories" />
+        <meta name="author" content="Svetlana Shkarina" />
+      </Head>
       <Hero />
       <About />
       {articles && <GalleryList articles={articles} />}
